e1cda412ec6c97cd195e2035312877049953b1a7
AdryDev <adrydevmateo@gmail.com>
2024-04-16 22:27:39 -0400

    Fixed status code sent from user controllers

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -14,14 +14,14 @@ export class UserController {
 	@Post()
 	async CreateUser(@Body() reqBody: TUserDTO): Promise<TUserCRUDResponse> {
 		const created = await this.service.CreateUser(reqBody)
-		if (!created.OK) return { msg: created.msg, statusCode: HttpStatus.CONTINUE, OK: false }
+		if (!created.OK) return { msg: created.msg, statusCode: HttpStatus.UNAUTHORIZED, OK: false }
 		return { msg: created.msg, OK: true, statusCode: HttpStatus.OK }
 	}
 
 	@Post('sign-in')
 	async SignIn(@Body() reqBody: TUserDTO): Promise<TUserCRUDResponse> {
 		const authorized = await this.service.SignIn(reqBody)
-		if (!authorized.OK) return { msg: authorized.msg, statusCode: HttpStatus.CONTINUE, OK: false }
+		if (!authorized.OK) return { msg: authorized.msg, statusCode: HttpStatus.UNAUTHORIZED, OK: false }
 		return { msg: authorized.msg, OK: true, data: authorized.data, statusCode: HttpStatus.OK }
 	}
 
@@ -30,7 +30,7 @@ export class UserController {
 		if (!reqBody.token) return { msg: '[Missing Field]: token was not provided', OK: false }
 		if (typeof reqBody.token !== 'string') return { msg: '[Invalid Type]: token is not a string', OK: false }
 		const updated = await this.service.UpdateUser(reqBody)
-		if (!updated.OK) return { msg: updated.msg, statusCode: HttpStatus.CONTINUE, OK: false }
+		if (!updated.OK) return { msg: updated.msg, statusCode: HttpStatus.UNAUTHORIZED, OK: false }
 		return { msg: updated.msg, OK: true, statusCode: HttpStatus.OK }
 	}
 
@@ -39,7 +39,7 @@ export class UserController {
 		if (!reqBody.token) return { msg: '[Missing Field]: token was not provided', OK: false }
 		if (typeof reqBody.token !== 'string') return { msg: '[Invalid Type]: token is not a string', OK: false }
 		const deleted = await this.service.DeleteUser(reqBody)
-		if (!deleted.OK) return { msg: deleted.msg, statusCode: HttpStatus.CONTINUE, OK: false }
+		if (!deleted.OK) return { msg: deleted.msg, statusCode: HttpStatus.UNAUTHORIZED, OK: false }
 		return { msg: deleted.msg, OK: true, statusCode: HttpStatus.OK }
 	}
 }
